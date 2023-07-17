5649e31051534fd96ac3189e5e79f0dfb354dcaf
Hayato Miyamoto <falconmiyamoto@icloud.com>
2023-07-18 11:34:14 +1200

    working

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,12 @@
+import sys
+import os
+
+# Add parent directory to the Python path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+
 from app import app
 from flask import render_template, abort
 from flask_sqlalchemy import SQLAlchemy
