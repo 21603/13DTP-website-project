d50011d0790221fdcd62eb8ba004ae060445a009
Hayato Miyamoto <falconmiyamoto@icloud.com>
2023-07-18 11:30:59 +1200

    making the code into the teacher's code structure

---
+++ app/models.py
@@ -0,0 +1,52 @@
+from app.routes import db
+
+# Association table to represent the many-to-many relationship between Meal and Ingredient
+MealIngredient = db.Table('MealIngredient',
+    db.Column('mid', db.Integer, db.ForeignKey('Meal.id')),
+    db.Column('iid', db.Integer, db.ForeignKey('Ingredient.id'))
+)
+
+class Base(db.Model):
+    __tablename__ = "Base"
+
+    # Columns for the Base table
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    description = db.Column(db.Text())
+    image_url = db.Column(db.Text())
+
+    def __repr__(self):
+        return self.name
+
+class Meal(db.Model):
+    __tablename__ = "Meal"
+
+    # Columns for the Meal table
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    description = db.Column(db.Text())
+    price = db.Column(db.Text())
+    image_url = db.Column(db.Text())
+
+    # Foreign key relationship to the Base table
+    base_id = db.Column(db.Integer, db.ForeignKey('Base.id'))
+
+    # Establishing a relationship with the Base table (one-to-many)
+    base = db.relationship('Base', backref='meals')
+
+    # Establishing a relationship with the Ingredient table through the MealIngredient association table (many-to-many)
+    ingredients = db.relationship('Ingredient', secondary=MealIngredient, backref='meals')
+
+    def __repr__(self):
+        return f'{self.name.upper()} MEAL'
+
+class Ingredient(db.Model):
+    __tablename__ = "Ingredient"
+
+    # Columns for the Ingredient table
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    description = db.Column(db.Text())
+
+    def __repr__(self):
+        return self.name
---
+++ app/routes.py
@@ -0,0 +1,47 @@
+from app import app
+from flask import render_template, abort
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+db = SQLAlchemy()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, "Japanese food.db")
+db.init_app(app)
+
+import app.models as models
+
+# basic route
+@app.route('/')
+def root():
+    return render_template('home.html', page_title='HOME')
+
+# about route
+@app.route('/about')
+def about():
+    return render_template('about.html', page_title='ABOUT')
+
+# Page to display all Japanese food and link to each individual one
+@app.route('/all_Japanese_food')
+def all_Japanese_food():
+    Japanese_food = models.Meal.query.all()
+    return render_template("all_Japanese_food.html", Japanese_food=Japanese_food)
+
+# Display the details of one Japanese food including its toppings
+@app.route('/Japanese_food/<int:id>')
+def Japanese_food(id):
+    # Get the Japanese food, but throw a 404 if the id doesn't exist
+    Japanese_food = models.Meal.query.filter_by(id=id).first_or_404()
+    print(Japanese_food, Japanese_food.base)  # DEBUG
+    return render_template('Japanese_food.html', Japanese_food=Japanese_food)
+
+@app.route('/base/<int:id>')
+def base(id):
+    Japanese_food = models.Meal.query.filter_by(base_id=id).all()
+    return render_template("base.html", Japanese_food=Japanese_food)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
